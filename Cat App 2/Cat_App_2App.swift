54cc032aae65fa9e242f3dfca5c216de80a0e329
DWizard11 <daniyalisawesome111@gmail.com>
2022-07-30 09:51:17 +0800

    Initial Commit

---
+++ Cat App 2/Cat_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_App_2App.swift
+//  Cat App 2
+//
+//  Created by DWizard11 on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Cat_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
